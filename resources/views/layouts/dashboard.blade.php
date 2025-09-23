a2330f7d04df466bffec80338999c4a798778b20
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-09-23 22:48:55 +0100

    update color

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,13 +11,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="{{ asset('storage/fontawesome-pro/fontawesome-pro-6.5.2-web/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('storage/fontawesome-pro/css/all.min.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     
     <style>
         :root {
-            --primary-color: #ff6b35;
-            --secondary-color: #f7931e;
+            --primary-color: #ac2e00;
+            --secondary-color: #f4e005;
             --success-color: #28a745;
             --danger-color: #dc3545;
             --warning-color: #ffc107;
