193c0cf830dd1474843e1c0e2994d0117b3849cd
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-02-17 13:59:40 +0100

    fix Payment Gateway

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     ],
 
     'paystack' => [
-        'base_url' => env('PAYSTACK_PAYMENT_URL'),
+        'base_url' => env('PAYSTACK_PAYMENT_URL', 'https://api.paystack.co'),
         'public_key' => env('PAYSTACK_PUBLIC_KEY'),
         'secret_key' => env('PAYSTACK_SECRET_KEY'),
         'merchant_email' => env('MERCHANT_EMAIL'),
