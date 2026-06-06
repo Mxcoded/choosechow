de261723779d821b11d42936d88084083a331598
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-06-06 20:57:30 +0100

    Update Seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -9,18 +9,20 @@ class AdminUserSeeder extends Seeder
 {
     public function run()
     {
-        // 1. Create the User
-        $user = User::create([
-            'first_name' => 'Mezabu',
-            'last_name' => 'Xcoded',
-            'email' => 'admin@choosechow.com',
-            'password' => '#Pwd123#', // Model mutator will hash this automatically
-            'phone' => '1234567890',
-            'status' => 'active',
-            'email_verified_at' => now(),
-        ]);
+        $user = User::firstOrCreate(
+            ['email' => 'admin@choosechow.com'],
+            [
+                'first_name' => 'Mezabu',
+                'last_name' => 'Xcoded',
+                'password' => '#Pwd123#',
+                'phone' => '1234567890',
+                'status' => 'active',
+                'email_verified_at' => now(),
+            ]
+        );
 
-        // 2. Assign the Spatie Role
-        $user->assignRole('admin');
+        if (!$user->hasRole('admin')) {
+            $user->assignRole('admin');
+        }
     }
 }
